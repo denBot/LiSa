0d6fcdacb74d305a1eba94c35186a395de5f79e6
denBot <den.xyz@pm.me>
2021-02-24 11:26:20 +0000

    Configure docker-compose.yml to read default passes from .env

--- docker/worker/init.sh
+++ docker/worker/init.sh
@@ -28,4 +28,5 @@ su - lisa -c "API_SUCCESS_URL=\"$API_SUCCESS_URL\" \
               VIRUSTOTAL_API_KEY=\"$VIRUSTOTAL_API_KEY\" \
               RABBITMQ_DEFAULT_PASS=\"$RABBITMQ_DEFAULT_PASS\" \
               MYSQL_PASSWORD=\"$MYSQL_PASSWORD\" \
+              BROKER_HEARTBEAT=0 \
               celery -A lisa.web_api.tasks worker --loglevel=info --concurrency=1 -n lisa-worker@%h"
